5aa9d82476429890b8e99066182b32270bd9626e
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-02-07 03:14:16 +0000

    Done HelloWorldStyle swift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,14 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Remy Skelton 
+//  Created on 2025-February-05
+//  Version 1.0
+//  Copyright (c) Remy Skelton. All rights reserved.
+//
+//  This program says hello to someone and an additional statement.
+
+// Display a greeting message + statement
+
+print("Hello, Santiago")
+print("How is the new semester?")
